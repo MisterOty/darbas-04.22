5ee22d897137191f8d0eebe9528c678362425ad6
MisterOty <oskarasvenzlauskas@gmail.com>
2026-04-22 12:56:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-//1. Ispausdinti kiekvieno post pavadinima
-//2. Sukurti posts elementa puslapyje
-//3. Sukurti elementa kiekvienam postui su duomenimis
-
 async function getPost(){
     let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     response = await response.json()
     console.log(response.userId, response.id, response.title, response.completed)
 }
 
-async function getPosts(){
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    response = await response.json()
-    return response
+async function getPosts() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    response = await response.json();
+    return response;
+}
+
+async function getUsers() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+    response = await response.json();
+    return response;
+}
+
+function getUserByID(users, id) {
+    return users.find(user => user.id == id);
 }
 
 window.onload = async() => {
     const posts = await getPosts()
+    const users = await getUsers()
     for(let i = 0; i < posts.length; i++){
+        let user = getUserByID(users, posts[i].userId)
         document.querySelector('main').innerHTML += `
         <div class="container num${i}">
-            <h1>Number ${posts[i].id}</h1>
-            <p><b>(User ID: ${posts[i].userId})</b> ${posts[i].title}</p>
+            <h3>(User: ${user.name})</h3>
+            <h1>${posts[i].title}</h1>
         </div>`
         if(posts[i].completed){
             document.querySelector(`.num${i}`).classList.add(`isCompleted`)
